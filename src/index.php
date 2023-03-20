5250d3efa03ce42dc21bbee39addba7c742d40b2
vpyojanny <102862361+vpyojanny@users.noreply.github.com>
2023-03-19 22:40:40 -0400

    Update index.php

--- src/index.php
+++ src/index.php
@@ -4,7 +4,7 @@
   	<meta charset="UTF-8">
   	<meta name="viewport" content="width=device-width, initial-scale=1.0">
   	<link rel="stylesheet" type="text/css" href="estilos.css">
-    <title>Asignacion S3 - AP</title>
+    <title>Asignacion S3 - AP3</title>
   </head>
   <body>
     <form action="modificar.php" method="post">
